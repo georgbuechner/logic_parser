42cdf47d9c6a5763bc1c7180f740d502e1491da9
fux <jan.dick@bdp.org>
2020-11-05 00:27:49 +0100

    Adds functionality to find matching bracket to first bracket

--- Evaluator.java
+++ Evaluator.java
@@ -15,6 +15,33 @@ public class Evaluator {
   public boolean Evaluate (String expression) {
     System.out.println("Expression: " + expression);
     System.out.println("Substitutes: " + substitutes_);
+
+    //Check if string is empty, if yes, return true
+    if (expression == "") 
+      return true;
+
+    //Check if elementary form but in brackets
+    if (expression.charAt(0) == '(' && MatchingBracket(expression) 
+        == expression.length()-1) {
+      expression = expression.substring(0, expression.length()-1);
+      expression = expression.substring(1, expression.length());
+    }
+    System.out.println("Expression: " + expression);
+
     return true;
+  }
+
+  private int MatchingBracket(String str) {
+    int num=0;
+    for (int i=0; i<str.length(); i++) {
+      if (str.charAt(i) == '(')
+        num++;
+      else if (str.charAt(i) == ')' && num == 1)
+        return i;
+      else if (str.charAt(i) == ')')
+        num--;
+    }
+    System.out.println("Serious error: MatchingBracket failed!");
+    return 0;
   }
 };
