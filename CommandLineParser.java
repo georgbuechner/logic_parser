f1d79360e74128beae68cd81dff08cd8e998a6e5
fux <jan.dick@bdp.org>
2020-11-04 23:55:01 +0100

    Addes small commandline parser

---
+++ CommandLineParser.java
@@ -0,0 +1,18 @@
+import java.util.Map;
+import java.util.HashMap;
+
+public class CommandLineParser {
+  Map<String, String> params_;
+  public CommandLineParser(Map<String, String> params) {
+    params_ = params; 
+  }
+
+  Map<String, String> GetParams(String[] args) {
+    Map<String, String> params = new HashMap<>();
+    for (int i=0; i<args.length; i++) {
+      if (params_.containsKey(args[i]) == true && args.length > i+1)
+        params.put(params_.get(args[i]), args[i+1]);
+    }
+    return params;
+  }
+}
