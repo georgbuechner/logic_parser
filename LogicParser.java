9e89747a5ed9e82b6f8edc829d9f4ca5fe38a37a
fux <jan.dick@bdp.org>
2020-11-04 22:46:08 +0100

    Added initial main

---
+++ LogicParser.java
@@ -0,0 +1,12 @@
+/**
+ * @author: georgbuechner
+ */
+
+public class LogicParser{
+  public static void main(String[] args) {
+    System.out.println("LogicParser called with arguments: ");
+    for (String s : args) {
+      System.out.println(s);
+    }
+  }
+}
